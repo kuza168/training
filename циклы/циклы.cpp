7ab6990997630b3137201c265725d12da68543a3
anlik <anlik@DESKTOP-E3G4560>
2021-09-10 08:47:55 +0500

    Добавьте файлы проекта.

---
+++ циклы/циклы.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i = 0;
+	int sum = 0;
+	do
+	{
+		i++;
+		sum += i;
+	} while (i < 5000);
+	cout << "Сумма чисел  от 1 до 5000 = " << sum << " конечное число = " << i << endl;
+	return 0;
+}
